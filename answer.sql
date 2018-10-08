bced5f5f244c80b5ffd48a7e704752a5e9d91d84
Soun Kim <kim500@pod1-2.cs.purdue.edu>
2018-10-08 15:20:24 -0400

    1&3 pass

--- answer.sql
+++ answer.sql
@@ -1,11 +1,11 @@
 -- Query1
-SELECT Category, COUNT(ProductID) AS NumberOfProducts FROM Product GROUP BY Category ORDER BY NumberOfProducts Desc;
+SELECT Category, COUNT(ProductID) AS NumberOfProducts FROM Product GROUP BY Category ORDER BY NumberOfProducts DESC;
 
 -- Query2
-
+SELECT Category,  ROUND(AVG(ExFactoryPrice),2) AS AveragePrice FROM Product GROUP BY Category ORDER BY AveragePrice DESC;
 
 -- Query3
-
+SELECT P.ProductId, P.ProductName FROM Product P, Company C WHERE P.CompanyId = C.CompanyId AND C.CompanyID IN (SELECT C.CompanyID FROM Company C WHERE C.State ='IN' OR C.State = 'CA'); 
 
 -- Query4
 
